3e5286563701fdf5866b1fbed3f73610c801c9d5
Dimi <diba0001@stud.kea.dk>
2025-04-02 21:31:30 +0200

    hompage image

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,17 @@
-export default function Home() {
+import React from 'react';
+
+const HomepageImage = () => {
   return (
-      <div>
-        <h1>home?</h1>
+    <div className="flex items-center justify-center h-screen w-full">
+      <div className="w-full max-w-4xl px-4">
+        <div className="aspect-video bg-gray-200 w-full">
+          <div className="w-full h-full flex items-center justify-center text-gray-500">
+            Homepage Image
+          </div>
+        </div>
       </div>
+    </div>
   );
-}
+};
+
+export default HomepageImage;
