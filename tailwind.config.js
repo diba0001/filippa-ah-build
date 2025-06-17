726336a2c0ee5ef83a9b69b55317521cb8c87b25
Dimi <mitri.media@hotmail.com>
2025-06-17 20:03:03 +0200

    Re-mounted the styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,9 @@
 module.exports = {
   content: [
     './src/**/*.{js,ts,jsx,tsx}',
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
