87fcad4ed80c810e2f77b7f84d03b206e0bc6546
Dimi <mitri.media@hotmail.com>
2025-08-12 21:01:07 +0200

    Trying with lowercase

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 
 const HomepageImage = ({ 
   images = [
-    "/images/GIFF1.jpg",
+    "/images/giff1.jpg",
     "/images/GIFF/GIFF2.jpg",
     "/images/GIFF/GIFF3.jpg",
     "/images/GIFF/GIFF4.jpg",
