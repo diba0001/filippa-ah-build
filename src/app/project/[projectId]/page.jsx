6aad0e1f2f6135eed30b0be786ecab93a35d0f46
Dimi <mitri.media@hotmail.com>
2025-08-27 10:29:54 +0200

    Back to basics

--- src/app/project/[projectId]/page.jsx
+++ src/app/project/[projectId]/page.jsx
@@ -6,27 +6,6 @@ import Link from 'next/link';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger'; 
 
-export async function generateStaticParams() {
-  return [
-    { projectId: 'YAS_WINTER_25' },
-    { projectId: 'VERO_MODA_COL.5_AW24' },
-    { projectId: 'YAS_SS5' },
-    { projectId: 'MAMALICIOUS_SS6' },
-    { projectId: 'YAS_SS6' },
-    { projectId: 'YAS_A25' },
-    { projectId: 'YAS_AW2' },
-    { projectId: 'IVY_CPH_AUTUMN_24' },
-    { projectId: 'IVY_CPH_SPRING_24' },
-    { projectId: 'MAMALICIOUS_BLACK_FRIDAY_2024' },
-    { projectId: 'MAMALICIOUS_AUTUMN_CAMPAIGN_AW1' },
-    { projectId: 'MAMALICIOUS_AUTUMN_CAMPAIGN_AW2' },
-    { projectId: 'PLENTY&_CAMPAIGN' },
-    { projectId: 'YAS-CELEBRATION' },
-    { projectId: 'YAS' },
-    { projectId: 'YAS_SS4' }
-  ];
-}
-
 gsap.registerPlugin(ScrollTrigger);
 
 const ProjectPage = ({ params }) => {
