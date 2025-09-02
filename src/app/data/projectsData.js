c1b64688e8de346287846f50b46b0fb319b9cd55
Dimi <mitri.media@hotmail.com>
2025-09-02 16:18:36 +0200

    Updated images to have the desired sequences

--- src/app/data/projectsData.js
+++ src/app/data/projectsData.js
@@ -6,10 +6,10 @@ export const projectsData = {
     description: "Project in collaboration with: Matthew Minton, Vivi Søderholm, Sofie Liv Madsen",
     images: [
       "/images/YAS_WINTER_25_CAMPAIGN/W1.jpg",
+      "/images/YAS_WINTER_25_CAMPAIGN/W5.jpg",
       "/images/YAS_WINTER_25_CAMPAIGN/W2.jpg",
       "/images/YAS_WINTER_25_CAMPAIGN/W3.jpg",
-      "/images/YAS_WINTER_25_CAMPAIGN/W4.jpg",
-      "/images/YAS_WINTER_25_CAMPAIGN/W5.jpg"
+      "/images/YAS_WINTER_25_CAMPAIGN/W4.jpg"
     ]
   },
 
@@ -18,13 +18,13 @@ export const projectsData = {
     role: "Art Direction",
     description: "Project in collaboration with: Jonas Lindhardt, Anders Witt, Sofie Liv Madsen, Kathie Lam",
     images: [
-      "/images/VERO_MODA_COL_5/VM_1.jpg",
-      "/images/VERO_MODA_COL_5/VM_2.jpg",
-      "/images/VERO_MODA_COL_5/VM_3.jpg",
-      "/images/VERO_MODA_COL_5/VM_4.jpg",
       "/images/VERO_MODA_COL_5/VM_5.jpg",
+      "/images/VERO_MODA_COL_5/VM_7.jpg",
+      "/images/VERO_MODA_COL_5/VM_3.jpg",
       "/images/VERO_MODA_COL_5/VM_6.jpg",
-      "/images/VERO_MODA_COL_5/VM_7.jpg"
+      "/images/VERO_MODA_COL_5/VM_4.jpg",
+      "/images/VERO_MODA_COL_5/VM_1.jpg",
+      "/images/VERO_MODA_COL_5/VM_2.jpg"
     ]
   },
 
@@ -33,27 +33,16 @@ export const projectsData = {
     role: "Art Direction",
     description: "Project in collaboration with: Matthew Minton, Vivi Søderholm, Sofie Liv Madsen, Floor Verbruggen",
     images: [
-      "/images/YAS_SS5/SS5_1.jpg",
-      "/images/YAS_SS5/SS5_2.jpg",
-      "/images/YAS_SS5/SS5_3.jpg",
       "/images/YAS_SS5/SS5_4.jpg",
       "/images/YAS_SS5/SS5_5.jpg",
-      "/images/YAS_SS5/SS5_6.jpg",
-      "/images/YAS_SS5/SS5_7.jpg",
-      "/images/YAS_SS5/SS5_8.jpg",
-      "/images/YAS_SS5/SS5_9.jpg",
-      "/images/YAS_SS5/SS5_10.jpg",
-      "/images/YAS_SS5/SS5_11.jpg",
-      "/images/YAS_SS5/SS5_12.jpg",
-      "/images/YAS_SS5/SS5_13.jpg",
-      "/images/YAS_SS5/SS5_14.jpg",
-      "/images/YAS_SS5/SS5_15.jpg",
-      "/images/YAS_SS5/SS5_16.jpg",
-      "/images/YAS_SS5/SS5_17.jpg",
       "/images/YAS_SS5/SS5_18.jpg",
-      "/images/YAS_SS5/SS5_19.jpg",
-      "/images/YAS_SS5/SS5_20.jpg",
-      "/images/YAS_SS5/SS5_21.jpg"
+      "/images/YAS_SS5/SS5_3.jpg",
+      "/images/YAS_SS5/SS5_2.jpg",
+      "/images/YAS_SS5/SS5_7.jpg",
+      "/images/YAS_SS5/SS5_11.jpg",
+      "/images/YAS_SS5/SS5_16.jpg",
+      "/images/YAS_SS5/SS5_13.jpg",
+      "/images/YAS_SS5/SS5_1.jpg"
     ]
   },
 
@@ -62,13 +51,12 @@ export const projectsData = {
     role: "Art Direction & Styling",
     description: "Project in collaboration with: Anders Witt, Sofie Samuelsen, Mads Stig, Anne-Cecilie Gade, Nicole Rijsdijk",
     images: [
-      "/images/MAMALICIOUS_SS6_2025/SS6_1.jpg",
-      "/images/MAMALICIOUS_SS6_2025/SS6_2.jpg",
-      "/images/MAMALICIOUS_SS6_2025/SS6_3.jpg",
-      "/images/MAMALICIOUS_SS6_2025/SS6_4.jpg",
+      "/images/MAMALICIOUS_SS6_2025/SS6_7.jpg",
       "/images/MAMALICIOUS_SS6_2025/SS6_5.jpg",
-      "/images/MAMALICIOUS_SS6_2025/SS6_6.jpg",
-      "/images/MAMALICIOUS_SS6_2025/SS6_7.jpg"
+      "/images/MAMALICIOUS_SS6_2025/SS6_3.jpg",
+      "/images/MAMALICIOUS_SS6_2025/SS6_2.jpg",
+      "/images/MAMALICIOUS_SS6_2025/SS6_1.jpg",
+      "/images/MAMALICIOUS_SS6_2025/SS6_4.jpg"
     ]
   },
 
@@ -77,20 +65,18 @@ export const projectsData = {
     role: "Art Direction",
     description: "Project in collaboration with: Christian Torp, Jan Bue Laumark, Vivi Søderholm, Maja Westergaard, Alberte Skovmose",
     images: [
-      "/images/YAS_SS6/SS6_1.jpg",
-      "/images/YAS_SS6/SS6_2.jpg",
-      "/images/YAS_SS6/SS6_3.jpg",
-      "/images/YAS_SS6/SS6_4.jpg",
-      "/images/YAS_SS6/SS6_5.jpg",
-      "/images/YAS_SS6/SS6_6.jpg",
-      "/images/YAS_SS6/SS6_7.jpg",
       "/images/YAS_SS6/SS6_8.jpg",
+      "/images/YAS_SS6/SS6_2.jpg",
       "/images/YAS_SS6/SS6_9.jpg",
-      "/images/YAS_SS6/SS6_10.jpg",
-      "/images/YAS_SS6/SS6_11.jpg",
+      "/images/YAS_SS6/SS6_6.jpg",
       "/images/YAS_SS6/SS6_12.jpg",
       "/images/YAS_SS6/SS6_13.jpg",
-      "/images/YAS_SS6/SS6_14.jpg"
+      "/images/YAS_SS6/SS6_14.jpg",
+      "/images/YAS_SS6/SS6_7.jpg",
+      "/images/YAS_SS6/SS6_10.jpg",
+      "/images/YAS_SS6/SS6_1.jpg",
+      "/images/YAS_SS6/SS6_4.jpg",
+      "/images/YAS_SS6/SS6_11.jpg"
     ]
   },
 
@@ -99,16 +85,16 @@ export const projectsData = {
     role: "Art Direction",
     description: "Project in collaboration with: Christian Torp, Sofie Blicher",
     images: [
-      "/images/YAS_AUTUMN_25_CAMPAIGN/A25_1.jpg",
-      "/images/YAS_AUTUMN_25_CAMPAIGN/A25_2.jpg",
-      "/images/YAS_AUTUMN_25_CAMPAIGN/A25_3.jpg",
-      "/images/YAS_AUTUMN_25_CAMPAIGN/A25_4.jpg",
-      "/images/YAS_AUTUMN_25_CAMPAIGN/A25_5.jpg",
-      "/images/YAS_AUTUMN_25_CAMPAIGN/A25_6.jpg",
-      "/images/YAS_AUTUMN_25_CAMPAIGN/A25_7.jpg",
-      "/images/YAS_AUTUMN_25_CAMPAIGN/A25_8.jpg",
       "/images/YAS_AUTUMN_25_CAMPAIGN/A25_9.jpg",
-      "/images/YAS_AUTUMN_25_CAMPAIGN/A25_10.jpg"
+      "/images/YAS_AUTUMN_25_CAMPAIGN/A25_4.jpg",
+      "/images/YAS_AUTUMN_25_CAMPAIGN/A25_2.jpg",
+      "/images/YAS_AUTUMN_25_CAMPAIGN/A25_6.jpg",
+      "/images/YAS_AUTUMN_25_CAMPAIGN/A25_8.jpg",
+      "/images/YAS_AUTUMN_25_CAMPAIGN/A25_10.jpg",
+      "/images/YAS_AUTUMN_25_CAMPAIGN/A25_7.jpg",
+      "/images/YAS_AUTUMN_25_CAMPAIGN/A25_3.jpg",
+      "/images/YAS_AUTUMN_25_CAMPAIGN/A25_1.jpg",
+      "/images/YAS_AUTUMN_25_CAMPAIGN/A25_5.jpg"
     ]
   },
 
@@ -117,14 +103,14 @@ export const projectsData = {
     role: "Art Direction & Styling",
     description: "Project in collaboration with: Christian Torp, Vivi Søderholm, Maja Westergaard, Amanda Casagrande",
     images: [
-      "/images/YAS_AW2/AW2_1.jpg",
-      "/images/YAS_AW2/AW2_2.jpg",
-      "/images/YAS_AW2/AW2_3.jpg",
-      "/images/YAS_AW2/AW2_4.jpg",
-      "/images/YAS_AW2/AW2_5.jpg",
-      "/images/YAS_AW2/AW2_6.jpg",
       "/images/YAS_AW2/AW2_7.jpg",
-      "/images/YAS_AW2/AW2_8.jpg"
+      "/images/YAS_AW2/AW2_4.jpg",
+      "/images/YAS_AW2/AW2_6.jpg",
+      "/images/YAS_AW2/AW2_2.jpg",
+      "/images/YAS_AW2/AW2_8.jpg",
+      "/images/YAS_AW2/AW2_3.jpg",
+      "/images/YAS_AW2/AW2_5.jpg",
+      "/images/YAS_AW2/AW2_1.jpg"
     ]
   },
 
@@ -133,13 +119,13 @@ export const projectsData = {
     role: "Art Direction & Styling",
     description: "Project in collaboration with: Matthew Minton, Josefine Gottwald Munk, Sanne Moth",
     images: [
-      "/images/IVY_CPH_AUTUMN_24_CAMPAIGN/AW24_1.jpg",
       "/images/IVY_CPH_AUTUMN_24_CAMPAIGN/AW24_2.jpg",
+      "/images/IVY_CPH_AUTUMN_24_CAMPAIGN/AW24_1.jpg",
+      "/images/IVY_CPH_AUTUMN_24_CAMPAIGN/AW24_5.jpg",
+      "/images/IVY_CPH_AUTUMN_24_CAMPAIGN/AW24_7.jpg",
       "/images/IVY_CPH_AUTUMN_24_CAMPAIGN/AW24_3.jpg",
       "/images/IVY_CPH_AUTUMN_24_CAMPAIGN/AW24_4.jpg",
-      "/images/IVY_CPH_AUTUMN_24_CAMPAIGN/AW24_5.jpg",
-      "/images/IVY_CPH_AUTUMN_24_CAMPAIGN/AW24_6.jpg",
-      "/images/IVY_CPH_AUTUMN_24_CAMPAIGN/AW24_7.jpg"
+      "/images/IVY_CPH_AUTUMN_24_CAMPAIGN/AW24_6.jpg"
     ]
   },
 
@@ -165,17 +151,17 @@ export const projectsData = {
     role: "Art Direction Styling",
     description: "Project in collaboration with: Anders Witt, Sofie Samuelsen, Mille Erikstrup, Anne-Cecilie Gade, Nicoline Edinger",
     images: [
-      "/images/MAMALICIOUS_BLACK_FRIDAY_2024/BF_1.jpg",
+      "/images/MAMALICIOUS_BLACK_FRIDAY_2024/BF_11.jpg",
+      "/images/MAMALICIOUS_BLACK_FRIDAY_2024/BF_7.jpg",
       "/images/MAMALICIOUS_BLACK_FRIDAY_2024/BF_2.jpg",
+      "/images/MAMALICIOUS_BLACK_FRIDAY_2024/BF_10.jpg",
+      "/images/MAMALICIOUS_BLACK_FRIDAY_2024/BF_8.jpg",
       "/images/MAMALICIOUS_BLACK_FRIDAY_2024/BF_3.jpg",
+      "/images/MAMALICIOUS_BLACK_FRIDAY_2024/BF_1.jpg",
       "/images/MAMALICIOUS_BLACK_FRIDAY_2024/BF_4.jpg",
       "/images/MAMALICIOUS_BLACK_FRIDAY_2024/BF_5.jpg",
-      "/images/MAMALICIOUS_BLACK_FRIDAY_2024/BF_6.jpg",
-      "/images/MAMALICIOUS_BLACK_FRIDAY_2024/BF_7.jpg",
-      "/images/MAMALICIOUS_BLACK_FRIDAY_2024/BF_8.jpg",
       "/images/MAMALICIOUS_BLACK_FRIDAY_2024/BF_9.jpg",
-      "/images/MAMALICIOUS_BLACK_FRIDAY_2024/BF_10.jpg",
-      "/images/MAMALICIOUS_BLACK_FRIDAY_2024/BF_11.jpg"
+      "/images/MAMALICIOUS_BLACK_FRIDAY_2024/BF_6.jpg"
     ]
   },
 
@@ -184,15 +170,15 @@ export const projectsData = {
     role: "Art Direction",
     description: "Project in collaboration with: Anders Witt, Sofie Samuelsen, Mille Erikstrup, Anne-Cecilie Gade",
     images: [
+      "/images/MAMALICIOUS_AW1_2024/AW1_9.jpg",
       "/images/MAMALICIOUS_AW1_2024/AW1_1.jpg",
-      "/images/MAMALICIOUS_AW1_2024/AW1_2.jpg",
-      "/images/MAMALICIOUS_AW1_2024/AW1_3.jpg",
-      "/images/MAMALICIOUS_AW1_2024/AW1_4.jpg",
       "/images/MAMALICIOUS_AW1_2024/AW1_5.jpg",
       "/images/MAMALICIOUS_AW1_2024/AW1_6.jpg",
+      "/images/MAMALICIOUS_AW1_2024/AW1_3.jpg",
+      "/images/MAMALICIOUS_AW1_2024/AW1_2.jpg",
       "/images/MAMALICIOUS_AW1_2024/AW1_7.jpg",
-      "/images/MAMALICIOUS_AW1_2024/AW1_8.jpg",
-      "/images/MAMALICIOUS_AW1_2024/AW1_9.jpg"
+      "/images/MAMALICIOUS_AW1_2024/AW1_4.jpg",
+      "/images/MAMALICIOUS_AW1_2024/AW1_8.jpg"
     ]
   },
 
@@ -233,14 +219,14 @@ export const projectsData = {
     role: "Art Direction & Styling",
     description: "Project in collaboration with: Christian Torp, Jan Bue Laumark, Vivi Søderholm, Maja Westergaard, Caroline Forsling",
     images: [
-      "/images/YAS_CELEBRATION/DSCF6257.jpg",
-      "/images/YAS_CELEBRATION/DSCF6612.jpg",
-      "/images/YAS_CELEBRATION/DSCF6425.jpg",
-      "/images/YAS_CELEBRATION/DSCF6520.jpg",
       "/images/YAS_CELEBRATION/DSCF6527.jpg",
-      "/images/YAS_CELEBRATION/DSCF6561.jpg",
       "/images/YAS_CELEBRATION/DSCF6610.jpg",
-      "/images/YAS_CELEBRATION/DSCF6301.jpg"
+      "/images/YAS_CELEBRATION/DSCF6301.jpg",
+      "/images/YAS_CELEBRATION/DSCF6257.jpg",
+      "/images/YAS_CELEBRATION/DSCF6425.jpg",
+      "/images/YAS_CELEBRATION/DSCF6612.jpg",
+      "/images/YAS_CELEBRATION/DSCF6520.jpg",
+      "/images/YAS_CELEBRATION/DSCF6561.jpg"
     ]
   },
 
@@ -249,13 +235,18 @@ export const projectsData = {
     role: "Art Direction",
     description: "Project in collaboration with: Christian Torp, Jan Bue Laumark, Vivi Søderholm, Maja Westergaard, Caroline Forsling",
     images: [
-      "/images/YAS_AW1/250519_YAS_2DAGE21458.jpg",
-      "/images/YAS_AW1/250519_YAS_2DAGE20407.jpg",
       "/images/YAS_AW1/250519_YAS_2DAGE20870.jpg",
       "/images/YAS_AW1/250519_YAS_2DAGE20105-1.jpg",
-      "/images/YAS_AW1/250519_YAS_2DAGE20443.jpg",
+      "/images/YAS_AW1/250519_YAS_2DAGE21506.jpg",
       "/images/YAS_AW1/250519_YAS_2DAGE20893.jpg",
-      "/images/YAS_AW1/250519_YAS_2DAGE21395.jpg"
+      "/images/YAS_AW1/250519_YAS_2DAGE20716.jpg",
+      "/images/YAS_AW1/250519_YAS_2DAGE21603-1.jpg",
+      "/images/YAS_AW1/250519_YAS_2DAGE20443.jpg",
+      "/images/YAS_AW1/250519_YAS_2DAGE20577.jpg",
+      "/images/YAS_AW1/250519_YAS_2DAGE21395.jpg",
+      "/images/YAS_AW1/250519_YAS_2DAGE21458.jpg",
+      "/images/YAS_AW1/250519_YAS_2DAGE20407.jpg",
+      "/images/YAS_AW1/250519_YAS_2DAGE20503.jpg"
     ]
   },
 
@@ -264,19 +255,19 @@ export const projectsData = {
     role: "Art Direction & Styling",
     description: "A complete wardrobe transformation focusing on powerful, professional pieces...",
     images: [
-      "/images/YAS_SS4/SS4_1.jpg",
-      "/images/YAS_SS4/SS4_2.jpg",
-      "/images/YAS_SS4/SS4_3.jpg",
-      "/images/YAS_SS4/SS4_4.jpg",
-      "/images/YAS_SS4/SS4_5.jpg",
-      "/images/YAS_SS4/SS4_6.jpg",
-      "/images/YAS_SS4/SS4_7.jpg",
-      "/images/YAS_SS4/SS4_8.jpg",
-      "/images/YAS_SS4/SS4_9.jpg",
       "/images/YAS_SS4/SS4_10.jpg",
+      "/images/YAS_SS4/SS4_6.jpg",
       "/images/YAS_SS4/SS4_11.jpg",
+      "/images/YAS_SS4/SS4_5.jpg",
+      "/images/YAS_SS4/SS4_9.jpg",
+      "/images/YAS_SS4/SS4_13.jpg",
+      "/images/YAS_SS4/SS4_4.jpg",
+      "/images/YAS_SS4/SS4_2.jpg",
+      "/images/YAS_SS4/SS4_7.jpg",
+      "/images/YAS_SS4/SS4_3.jpg",
       "/images/YAS_SS4/SS4_12.jpg",
-      "/images/YAS_SS4/SS4_13.jpg"
+      "/images/YAS_SS4/SS4_1.jpg",
+      "/images/YAS_SS4/SS4_8.jpg"
     ]
   }
 };
